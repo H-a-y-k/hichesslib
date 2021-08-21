56ed8e45442c6bb98cee48224d2ac7c4fa8b52c2
H-a-y-K <agokugtw@gmail.com>
2021-08-21 22:21:51 +0400

    Initial commit

---
+++ chessplusplus_global.h
@@ -0,0 +1,18 @@
+#ifndef CHESSPLUSPLUS_GLOBAL_H
+#define CHESSPLUSPLUS_GLOBAL_H
+
+#if defined(_MSC_VER) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#  define Q_DECL_EXPORT __declspec(dllexport)
+#  define Q_DECL_IMPORT __declspec(dllimport)
+#else
+#  define Q_DECL_EXPORT     __attribute__((visibility("default")))
+#  define Q_DECL_IMPORT     __attribute__((visibility("default")))
+#endif
+
+#if defined(CHESSPLUSPLUS_LIBRARY)
+#  define CHESSPLUSPLUS_EXPORT Q_DECL_EXPORT
+#else
+#  define CHESSPLUSPLUS_EXPORT Q_DECL_IMPORT
+#endif
+
+#endif // CHESSPLUSPLUS_GLOBAL_H
---
+++ constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+
+}
+
+#endif // CONSTANTS_H
